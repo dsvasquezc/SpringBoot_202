017d06be1ce4486da477a8fe4f2a2364378bc888
dvasquezc <dsvasquezc@gmail.com>
2020-03-09 17:23:28 -0400

    only song class

---
+++ src/main/java/com/example/arti1/Song.java
@@ -0,0 +1,10 @@
+package com.example.arti1;
+
+public class Song {
+    private long id;
+    private String name;
+    private String artist;
+    private String album;
+    private int rating;
+    private int year;
+}
